d37d549039745827c4f7f4ff99bc210804a669f3
sinlimitestv25-coder <sinlimitestv25@gmail.com>
2026-08-15 00:26:21 -0300

    Nexo v0.3.1 (2/2)
    compilación actualizada para Netlify

--- dist/server/__vite_rsc_assets_manifest.js
+++ dist/server/__vite_rsc_assets_manifest.js
@@ -1,18 +1,18 @@
 export default {
-  "bootstrapScriptContent": "import(\"/_next/static/chunks/index-CnVDbMXu.js\")",
+  "bootstrapScriptContent": "import(\"/_next/static/chunks/index-BMuCuTti.js\")",
   "clientReferenceDeps": {
     "0f507ce17f9b": {
       "js": [
-        "/_next/static/chunks/AppShell-8FizC4Ik.js",
+        "/_next/static/chunks/AppShell-Bx9-nEeU.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-BgSIrAUN.js",
-        "/_next/static/chunks/index-CnVDbMXu.js"
+        "/_next/static/chunks/index-BMuCuTti.js"
       ],
       "css": []
     },
     "9276801271d6": {
       "js": [
-        "/_next/static/chunks/index-CnVDbMXu.js",
+        "/_next/static/chunks/index-BMuCuTti.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-BgSIrAUN.js"
       ],
@@ -20,7 +20,7 @@ export default {
     },
     "0b874ad30386": {
       "js": [
-        "/_next/static/chunks/index-CnVDbMXu.js",
+        "/_next/static/chunks/index-BMuCuTti.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-BgSIrAUN.js"
       ],
@@ -28,7 +28,7 @@ export default {
     },
     "593f344dc510": {
       "js": [
-        "/_next/static/chunks/index-CnVDbMXu.js",
+        "/_next/static/chunks/index-BMuCuTti.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-BgSIrAUN.js"
       ],
@@ -36,7 +36,7 @@ export default {
     },
     "bad85346fc72": {
       "js": [
-        "/_next/static/chunks/index-CnVDbMXu.js",
+        "/_next/static/chunks/index-BMuCuTti.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-BgSIrAUN.js"
       ],
@@ -44,16 +44,16 @@ export default {
     },
     "15c18cfaeeff": {
       "js": [
-        "/_next/static/chunks/layout-segment-context-CqNMkggd.js",
+        "/_next/static/chunks/layout-segment-context-BdjXMp6v.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
-        "/_next/static/chunks/index-CnVDbMXu.js",
+        "/_next/static/chunks/index-BMuCuTti.js",
         "/_next/static/chunks/framework-BgSIrAUN.js"
       ],
       "css": []
     },
     "8c0f216c4604": {
       "js": [
-        "/_next/static/chunks/index-CnVDbMXu.js",
+        "/_next/static/chunks/index-BMuCuTti.js",
         "/_next/static/chunks/rolldown-runtime-C60lm6uB.js",
         "/_next/static/chunks/framework-BgSIrAUN.js"
       ],
@@ -64,7 +64,7 @@ export default {
     "app/layout.tsx": {
       "js": [],
       "css": [
-        "/_next/static/css/index.CPO06Pff.css"
+        "/_next/static/css/index.DxuQDHnj.css"
       ]
     }
   }
